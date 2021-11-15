cb4141b59a4185eef4ee91dd1037817c14de32fb
Geri <geraldozeqollari2@gmail.com>
2021-11-15 09:40:14 +0100

    last changes

--- project/resources/views/livewire/button02.blade.php
+++ project/resources/views/livewire/button02.blade.php
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
 </head>
 <body>
--- project/resources/views/livewire/navbar.blade.php
+++ project/resources/views/livewire/navbar.blade.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('css/page.css') }}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.css" />
